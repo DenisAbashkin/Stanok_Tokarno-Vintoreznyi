46f5c13ca883269e4b39d4490d4e0af1930f82ea
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2021-04-18 01:16:03 +0300

    Добавил комнату

---
+++ Tokarno-Vintoreznyi/Assets/InputControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControl : MonoBehaviour
+{
+
+    public GameObject cameraOrbit;
+
+    public float rotateSpeed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float height = rotateSpeed * Input.GetAxis("Mouse Y");
+            float width = rotateSpeed * Input.GetAxis("Mouse X");
+
+            if (cameraOrbit.transform.eulerAngles.z + height <= 0.1f || cameraOrbit.transform.eulerAngles.z + height >= 179.9f)
+                height = 0;
+            //if (cameraOrbit.transform.eulerAngles.y + width <= 0.1f || cameraOrbit.transform.eulerAngles.y + width >= 179.9f)
+            //    width = 0;
+            cameraOrbit.transform.eulerAngles = new Vector3(
+                cameraOrbit.transform.eulerAngles.x,
+                cameraOrbit.transform.eulerAngles.y + width,
+                cameraOrbit.transform.eulerAngles.z + height);
+        }
+
+        float scrollFactor = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollFactor != 0)
+        {
+            cameraOrbit.transform.localScale = cameraOrbit.transform.localScale * (1f - scrollFactor);
+        }
+    }
+}
